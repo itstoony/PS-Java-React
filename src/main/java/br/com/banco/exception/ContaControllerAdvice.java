db2e3266e61c69de3f702308d8cb3de4252c20ff
itstoony <toonyrenner@gmail.com>
2022-12-24 13:25:18 -0300

    dealing with datetimeException

--- src/main/java/br/com/banco/exception/ContaControllerAdvice.java
+++ src/main/java/br/com/banco/exception/ContaControllerAdvice.java
@@ -1,11 +1,14 @@
 package br.com.banco.exception;
 
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import javax.persistence.EntityNotFoundException;
-import java.sql.SQLIntegrityConstraintViolationException;
+import javax.servlet.http.HttpServletRequest;
+import java.time.DateTimeException;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 /**
  * Exception handler
@@ -13,26 +16,86 @@ import java.sql.SQLIntegrityConstraintViolationException;
 @RestControllerAdvice
 public class ContaControllerAdvice {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> tratarErro404() {
-        return ResponseEntity.notFound().build();
+    /**
+     * Date time exception response entity.
+     *
+     * @param ex Exception
+     * @param request request
+     * @return the response entity
+     */
+    @ExceptionHandler(DateTimeException.class)
+    public ResponseEntity<?> dateTimeException(DateTimeException ex, HttpServletRequest request) {
+
+        var error = DefaultError.builder()
+                .timestamp(System.currentTimeMillis())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error("Bad Request")
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(BAD_REQUEST).body(error);
     }
 
+    /**
+     * SaldoInsuficienteException response entity
+     *
+     * @param ex Exception
+     * @param request request
+     * @return the response entity
+     */
     @ExceptionHandler(SaldoInsuficienteException.class)
-    public ResponseEntity<?> tratarErroSaldo(SaldoInsuficienteException ex) {
-        var erros = ex.getMessage();
-        return ResponseEntity.badRequest().body(erros);
+    public ResponseEntity<?> saldoException(SaldoInsuficienteException ex, HttpServletRequest request) {
+        var error = DefaultError.builder()
+                .timestamp(System.currentTimeMillis())
+                .status(BAD_REQUEST.value())
+                .error("Bad Request")
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+        return ResponseEntity.status(BAD_REQUEST).body(error);
     }
 
-    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public ResponseEntity<?> tratarErroDuplicataDb() {
-        var erros = "Entity already exists";
-        return ResponseEntity.badRequest().body(erros);
+    /**
+     * Illegal argument exception response entity.
+     *
+     * @param ex Exception
+     * @param request request
+     * @return the response entity
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> illegalArgumentException(IllegalArgumentException ex, HttpServletRequest request) {
+
+        var error = DefaultError.builder()
+                .timestamp(System.currentTimeMillis())
+                .status(BAD_REQUEST.value())
+                .error("Bad Request")
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(BAD_REQUEST).body(error);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> tratarArgumentoInvalido(MethodArgumentNotValidException ex) {
-        var erros = ex.getFieldErrors();
-        return ResponseEntity.badRequest().body(erros);
+    /**
+     * Object not found exception response entity.
+     *
+     * @param ex Exception
+     * @param request request
+     * @return the response entity
+     */
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<?> objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request) {
+
+        var error = DefaultError.builder()
+                .timestamp(System.currentTimeMillis())
+                .status(HttpStatus.NOT_FOUND.value())
+                .error("Not Found")
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(NOT_FOUND).body(error);
     }
+
 }
